7faa336e0249240b0bb42864ee1bf04753102824
Happiness Nneke <happymba11@yahoo.com>
2017-11-22 16:45:53 +0000

    first commit

---
+++ ecommerce/urls.py
@@ -0,0 +1,39 @@
+"""ecommerce URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+
+
+from django.conf.urls import url, include
+from django.contrib import admin
+from products import urls as products_urls
+from accounts import urls as accounts_urls
+from cart import urls as cart_urls
+from checkout import urls as urls_checkout
+from products.views import all_products
+from django.views import static
+from .settings import MEDIA_ROOT
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$',all_products, name="home"),
+    url(r'^products/',include(products_urls)),
+    url(r'^accounts/',include(accounts_urls)),
+    url(r'^cart/',include(cart_urls)),
+    url(r'^checkout/', include(urls_checkout)),
+    url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
+]
+
+
